eca1d9e58c0a9f7d26412c0de1df06ab2b7c861f
Armaan rawat <armaanrawat@Armaans-MacBook-Air.local>
2025-07-19 16:54:19 +0530

    updates

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -10,17 +10,44 @@ export class AuthService{
             .setProject(conf.appwriteProjectId)
         this.account = new Account(this.client);
     }
-    async createAccount({email,passwod,name}){
+    async createAccount({email,password,name}){
         try{
-           const userAccount = await this.account.create(ID.unique( ),email,passwod,name);
+           const userAccount = await this.account.create(ID.unique( ),email,password,name);
            if(userAccount){
             // call another method...
+            return this.login({email,password});
            }
            else{
             return userAccount;
            }
         }
         catch(error){
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }   
+        catch(error){
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
             throw error;
         }
     }
